9da1840e02caf2d9db76146e843d1dde3e2715ac
Thenspe <91274102+Thenspe@users.noreply.github.com>
2025-01-06 12:57:39 -0500

    add popup for location click, hopefully

--- aardeMap.js
+++ aardeMap.js
@@ -25,8 +25,13 @@ var options = {
 L.control.ruler(options).addTo(map);
 
 //click for coordinates
+var popup = L.popup();
+
 function onMapClick(e) {
-    alert("You clicked the map at " + e.latlng);
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
 }
 
 map.on('click', onMapClick);
