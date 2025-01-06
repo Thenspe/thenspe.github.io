7741c3937881baca0cd2807bf0e2115f7f752c8b
Thenspe <91274102+Thenspe@users.noreply.github.com>
2025-01-06 13:18:59 -0500

    turned coordinate popup back on, for building the geojson

--- aardeMap.js
+++ aardeMap.js
@@ -27,13 +27,13 @@ L.control.ruler(options).addTo(map);
 //click for coordinates
 var popup = L.popup();
 
-// function onMapClick(e) {
-//     popup
-//         .setLatLng(e.latlng)
-//         .setContent("You clicked the map at " + e.latlng.toString())
-//         .openOn(map);
-// };
-// map.on('click', onMapClick);
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+};
+map.on('click', onMapClick);
 
 // var geojsonFeature = {
 //     "type": "Feature",
