9951d3b3365371ef205cef276b812cadcb0e6233
Thenspe <91274102+Thenspe@users.noreply.github.com>
2025-01-06 12:41:38 -0500

    trying to fix the scale of the measure tool

--- aardeMap.js
+++ aardeMap.js
@@ -18,9 +18,16 @@ L.control.scale({
 var options = {
     position: 'topleft',
     lengthUnit: {
-        factor: 0.0,
         display: 'km',
-        decimal: 2,
+        factor: 0.003,
+        decimal: 1,
+        label: 'Days of Travel'
+    },
+    angleUnit: {
+        display: '&deg;',           // This is the display value will be shown on the screen. Example: 'Gradian'
+        decimal: 2,                 // Bearing result will be fixed to this value.
+        factor: null,                // This option is required to customize angle unit. Specify solid angle value for angle unit. Example: 400 (for gradian).
+        label: 'Bearing:'
     },
 };
 L.control.ruler(options).addTo(map);
