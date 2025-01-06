998eaff83b114c977de0a922d0076ee3de5586db
Thenspe <91274102+Thenspe@users.noreply.github.com>
2025-01-06 13:43:35 -0500

    Adjusting the zoom on setview

--- aardeMap.js
+++ aardeMap.js
@@ -2,7 +2,7 @@ var map = L.map('map', {
     crs: L.CRS.Simple,
     minZoom: 0
 }).setView([725,580]);
-var bounds = [[0,0],[1000,1000],1];
+var bounds = [[0,0],[1000,1000],8];
 var image = L.imageOverlay('images/Aarde_map.png',bounds).addTo(map);
 map.fitBounds(bounds);
 
