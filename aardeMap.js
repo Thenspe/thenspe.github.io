1b56730a5488f8168e2579d8b3bd2d540e2e3086
Thenspe <91274102+Thenspe@users.noreply.github.com>
2025-01-06 22:39:47 -0500

    change minzoom to -2

--- aardeMap.js
+++ aardeMap.js
@@ -1,8 +1,8 @@
-console.log("update 39");
+console.log("update 40");
 
 var map = L.map('map', {
     crs: L.CRS.Simple,
-    minZoom: 3,
+    minZoom: -2,
     zoomSnap: 0.25
 });
 var bounds = [[0,0],[1000,1000]];
