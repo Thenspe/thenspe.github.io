9d06f04ed71af951be8ab8af3f3db0a93398f3ab
Thenspe <91274102+Thenspe@users.noreply.github.com>
2025-01-13 20:15:31 -0500

    element positioning

--- mapitems.js
+++ mapitems.js
@@ -50,4 +50,68 @@ let towns = {
 // var roads = {};
 // var ruins = {};
 // var politicalBoundaries = {};
-var factionAssets = {};
+var factionAssets = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "TBoE",
+                "colour": "red",
+                "icon":"BoI"
+            },
+            "geometry": {
+                "type":"Point",
+                "coordinates": [518.057152,716.756712]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "VidarLeague",
+                "colour": "blue",
+                "icon":"BoI"
+            },
+            "geometry": {
+                "type":"Point",
+                "coordinates": [585.963797,646.51301]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Mercenaries",
+                "colour": "yellow",
+                "icon":"BoI"
+            },
+            "geometry": {
+                "type":"Kloshtaggen",
+                "coordinates": [585.963797,646.51301]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Kloshtaggen",
+                "colour": "purple",
+                "icon":"BoI"
+            },
+            "geometry": {
+                "type":"Point",
+                "coordinates": [585.963797,646.51301]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Artefactors",
+                "colour": "pink",
+                "icon":"BoI"
+            },
+            "geometry": {
+                "type":"Point",
+                "coordinates": [585.963797,646.51301]
+            }
+        }
+    ]
+};
