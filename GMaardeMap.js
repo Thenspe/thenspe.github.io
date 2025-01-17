16ed23261ef4cd312049556a496808be3a521cea
Thenspe <91274102+Thenspe@users.noreply.github.com>
2025-01-17 17:23:51 -0500

    110 trying to get the icons back at all

--- GMaardeMap.js
+++ GMaardeMap.js
@@ -1,4 +1,4 @@
-var updateNum = 109
+var updateNum = 110
 console.log("Update ",updateNum);
 var left = document.getElementById("left");
 
@@ -52,16 +52,16 @@ var fortIcon = L.icon({
 // Add the towns and villages
 const townsLayer = L.geoJSON(towns, {
     pointToLayer: function (feature, latlng) {
-        switch (feature.properties.size) {
-            case 'Fort':
-                return L.marker(latlng,{icon: fortIcon});
-            case 'Village':
+        // switch (feature.properties.size) {
+        //     case 'Fort':
+        //         return L.marker(latlng,{icon: fortIcon});
+        //     case 'Village':
                 return L.circleMarker(latlng, geojsonMarkerOptions,feature).on('click', function(e){
                 document.getElementById("title").innerHTML = feature.properties.name;
                 document.getElementById("population").innerHTML = feature.properties.population;
                 document.getElementById("information").innerHTML = feature.properties.info;
             });
-        }
+        // }
     },
     // onEachFeature: onEachFeature,
     maxZoom: 1
