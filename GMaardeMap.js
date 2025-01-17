fc64993f3f985cafcbfbe708e574dfc1d43e9d75
Thenspe <91274102+Thenspe@users.noreply.github.com>
2025-01-17 13:04:25 -0500

    fixed an error (added semicolon)

--- GMaardeMap.js
+++ GMaardeMap.js
@@ -1,4 +1,4 @@
-var updateNum = 58
+var updateNum = 59
 console.log("Update ",updateNum);
 
 var map = L.map('map', {
@@ -38,14 +38,14 @@ console.log("Zoom = ",map.getZoom());
 var popup = L.popup();
 
 function onMapClick(e) {
-    console.log("You clicked the map at " + e.latlng.toString(),'\n',"The current zoom is ", map.getZoom(),'\n',"Update ",updateNum)
+    console.log("You clicked the map at " + e.latlng.toString(),'\n',"The current zoom is ", map.getZoom(),'\n',"Update ",updateNum);
     // document.getElementById("ClickInfo").innerHTML = "You clicked the map at " + e.latlng.toString(),'\n',"The current zoom is ", map.getZoom(),'\n',"Update ",updateNum;
 };
 
 function onMapClick(e) {
     // console.log("You clicked the map at " + e.latlng.toString(),'\n',"The current zoom is ", map.getZoom(),'\n',"Update ",updateNum),
     // document.getElementById("ClickInfo").innerHTML = "You clicked the map at " + e.latlng.toString(),'\n',"The current zoom is ", map.getZoom(),'\n',"Update ",updateNum;
-    document.getElementById("ClickInfo").innerHTML = "You clicked on the map"
+    document.getElementById("ClickInfo").innerHTML = "You clicked on the map";
     
 };
 // add the geojson
