8ae22b004070893ef4c123d3372471016c69edb1
Thenspe <91274102+Thenspe@users.noreply.github.com>
2025-01-17 15:57:11 -0500

    88 trying to add click to map add

--- GMaardeMap.js
+++ GMaardeMap.js
@@ -1,4 +1,4 @@
-var updateNum = 87
+var updateNum = 88
 console.log("Update ",updateNum);
 var left = document.getElementById("left");
 
@@ -42,20 +42,24 @@ var geojsonMarkerOptions = {
     opacity: 1,
     fillOpacity: 1,
 };
-// function onEachFeature(feature, layer) {
-//     //checks each feature for the name of the town, and puts it in a popup
-//     if (feature.properties && feature.properties.name) {
-//         // layer.bindPopup(feature.properties.name);
-//         // document.getElementById("title").innerHTML = feature.properties.name;
-//         // document.getElementById("pop") = feature.properties.population;
-//         // document.getElementById("information") = feature.properties.info;
-//     }
-// }
+function onEachFeature(feature, layer) {
+    //checks each feature for the name of the town, and puts it in a popup
+    if (feature.properties && feature.properties.name) {
+        // layer.bindPopup(feature.properties.name);
+        // document.getElementById("title").innerHTML = feature.properties.name;
+        // document.getElementById("pop") = feature.properties.population;
+        // document.getElementById("information") = feature.properties.info;
+    }
+}
 
 // Add the towns and villages
 const townsLayer = L.geoJSON(towns, {
     pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, geojsonMarkerOptions,feature);
+        return L.circleMarker(latlng, geojsonMarkerOptions,feature).on('click', function(e){
+            document.getElementById("title").innerHTML = feature.properties.name;
+            document.getElementById("pop") = feature.properties.population;
+            document.getElementById("information") = feature.properties.info;
+        });
     },
     // onEachFeature: onEachFeature,
     maxZoom: 1
@@ -82,9 +86,9 @@ function onMapClick(e) {
 map.on('click', onMapClick);
 
 //Check if an object was clicked on, and display info if true
-townsLayer.on('click', function(e) {
-    console.log("it works when you click it");
-    document.getElementById("title").innerHTML = feature.properties.name;
-    document.getElementById("pop") = feature.properties.population;
-    document.getElementById("information") = feature.properties.info;
-} );
+// townsLayer.on('click', function(e) {
+//     console.log("it works when you click it");
+//     document.getElementById("title").innerHTML = feature.properties.name;
+//     document.getElementById("pop") = feature.properties.population;
+//     document.getElementById("information") = feature.properties.info;
+// } );
