6cbfad6d7971a393958e0d74eec0997fd0aae01d
S VanDam <stephen@vandam.ca>
2025-01-23 16:18:23 -0500

    leaflet ruler doubling distance, redoing measure factor

--- aardeMap.js
+++ aardeMap.js
@@ -1,4 +1,4 @@
-var updateNum = 142
+var updateNum = 143
 console.log("Update ",updateNum);
 var left = document.getElementById("left");
 
@@ -17,7 +17,7 @@ var options = {
     position: 'topleft',
     lengthUnit: {
         display: 'days',
-        factor: 0.00025, //set to 0.007 for km
+        factor: 0.007, //set to 0.007 for km
         decimal: 1,
         label: 'Distance:'
     },
