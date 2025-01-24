e9ade56f9e57a83f13f64f7efc0b5f6fc0fcc313
S VanDam <stephen@vandam.ca>
2025-01-24 15:57:54 -0500

    160 distance factor

--- aardeMap.js
+++ aardeMap.js
@@ -1,4 +1,4 @@
-var updateNum = 159
+var updateNum = 160
 console.log("Update ",updateNum);
 var left = document.getElementById("left");
 
@@ -17,7 +17,7 @@ var options = {
     position: 'topleft',
     lengthUnit: {
         display: 'km',
-        factor: 0.00025, //set to 0.007 for km
+        factor: 0.0003, //set to 0.007 for km
         decimal: 1,
         label: 'Distance:'
     },
