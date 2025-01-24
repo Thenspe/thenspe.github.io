883ce1f5e1ed31602fdd366ca1399c7de972247f
S VanDam <stephen@vandam.ca>
2025-01-24 16:07:44 -0500

    161 distance update

--- aardeMap.js
+++ aardeMap.js
@@ -1,4 +1,4 @@
-var updateNum = 160
+var updateNum = 161
 console.log("Update ",updateNum);
 var left = document.getElementById("left");
 
@@ -17,7 +17,7 @@ var options = {
     position: 'topleft',
     lengthUnit: {
         display: 'km',
-        factor: 0.0003, //set to 0.007 for km
+        factor: 0.0004, //set to 0.007 for km
         decimal: 1,
         label: 'Distance:'
     },
