9a637630799100e3bd14c7394a88e86689c517d0
S VanDam <stephen@vandam.ca>
2025-02-10 14:57:03 -0500

    Map items update - north and south Ablein

--- mapitems.js
+++ mapitems.js
@@ -5,7 +5,7 @@ let towns = {
                 "displayIcon": "iconsVillage",
                 "population": "2600",
                 "info": "Yarth is a small town at the edge of the Ruggengrat Mountains. Part of the kingdom of Danuthan, it boasts a few farms and some mining activity in the foothills.\nRecently the True Blood of Earth has sent a representative, Mehael, to try and proselytise their human-first messsage in the area.\nNeil has a contact here, and the local smith is a quiet but friendly fellow.",
-                "friends": "Neil's contact, village smith",
+                "friends": "Neil's contact (Winthrop), village smith",
                 "foes": "Mehael, of the True Blood of Earth."
             },
             "geometry": {
@@ -16,9 +16,9 @@ let towns = {
         {"type": "Feature","properties": {"name": "South Ablein",
                 "displayIcon": "iconsVillage",
                 "population": "9500",
-                "info": "unexplored",
-                "friends":"Haven't made any yet.",
-                "foes":"Haven't made any yet."
+                "info": "The southern portion of the city of Ablein is a diverse hub of activity, with humans, demihumans, and fae living peacefully together. Some separation is apparent, so you don't get a sense of harmonious living; neighbourhoods seem to be made up primarily of one group or another, but they all brush shoulders at the shops and in the squares without hostility. The buildings are roughly constructed but well-maintained, the streets are cramped but clean. The thoroughfares are filled with conversation, laughter, sorrow and joy. It is truly the shining jewel of the Danuthan Kingdom, although you'd never hear that from the rest of the kingdom.",
+                "friends":"Flisben, Tharukk, Neeral, Bostwich, Kaedan",
+                "foes":"Four surly thugs, possibly from the Carnifex."
             },
             "geometry": {
                 "type":"Point",
@@ -28,8 +28,8 @@ let towns = {
         {"type": "Feature","properties": {"name": "North Ablein",
                 "displayIcon": "iconsVillage",
                 "population": "5300",
-                "info": "unexplored",
-                "friends":"Haven't made any yet.",
+                "info": "The fancy upper city of Ablein, with wider streets and fresher air. The buildings are tall and in most cases, constructed with care and careful architecture. Exploring further reveals that the impressive buildings are merely ornate walls, keeping the world and its problems out of sight. There is litter in the corners, dark alleys that seem foreboding compared to the utilitarian feel of South Ablein. There are fewer people, fewer gatherings, more judgemental looks from people wearing more finery than you could in the southern half of the city.",
+                "friends":"Jaerstekk",
                 "foes":"Haven't made any yet."
             },
             "geometry": {
@@ -145,18 +145,6 @@ let towns = {
                 "coordinates": [680.570243,696.761771]
             }
         },
-        {"type": "Feature","properties": {"name": "blank",
-                "displayIcon": "iconsVillage", // iconsVillage, iconsFort
-                "population": "unknown",
-                "info": "unexplored",
-                "friends":"Haven't made any yet.",
-                "foes":"Haven't made any yet."
-            },
-            "geometry": {
-                "type":"Point",
-                "coordinates": [1,1]
-            }
-        },
         {"type": "Feature","properties": {"name": "Atchison",
             "displayIcon": "iconsCity",
             "population": "21, 000",
@@ -167,6 +155,18 @@ let towns = {
                 "geometry": {
                     "type":"Point",
                     "coordinates": [718.882071,749.859311]
+            }
+        },
+        {"type": "Feature","properties": {"name": "blank",
+                "displayIcon": "iconsVillage", // iconsVillage, iconsFort
+                "population": "unknown",
+                "info": "unexplored",
+                "friends":"Haven't made any yet.",
+                "foes":"Haven't made any yet."
+            },
+            "geometry": {
+                "type":"Point",
+                "coordinates": [1,1]
             }
         }
     ]
